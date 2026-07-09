d9a20865317e688218eba7ff646a2f831f34c996
padovaindonesia-bit <padovaindonesia@gmail.com>
2026-07-09 22:03:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function showAttendance() {
 
+document.getElementById("home").classList.remove("active");
+
+document.getElementById("attendance").classList.add("active");
+
+}
+
+function goHome() {
+
+document.getElementById("attendance").classList.remove("active");
+
+document.getElementById("home").classList.add("active");
+
+}
