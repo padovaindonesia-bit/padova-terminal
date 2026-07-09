fca0417f0fc13d1b7bc67032ec122f4f4af00869
padovaindonesia-bit <padovaindonesia@gmail.com>
2026-07-09 22:40:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-alert("script.js loaded");
-
 function showAttendance() {
-    alert("showAttendance called");
 
-    document.getElementById("home").classList.remove("active");
-    document.getElementById("attendance").classList.add("active");
+document.getElementById("home").classList.remove("active");
+
+document.getElementById("attendance").classList.add("active");
+
 }
 
 function goHome() {
-    alert("goHome called");
 
-    document.getElementById("attendance").classList.remove("active");
-    document.getElementById("home").classList.add("active");
+document.getElementById("attendance").classList.remove("active");
+
+document.getElementById("home").classList.add("active");
+
 }
